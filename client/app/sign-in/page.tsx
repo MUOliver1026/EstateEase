68cc8a4b9124ac0be41d280c4d3a1c5f3fafc0f0
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-03-22 11:54:09 +1100

    feat: header and router

---
+++ client/app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signin from '../components/signin';
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Sign In
+      </h1>
+      <Signin />
+    </div>
+  );
+}
