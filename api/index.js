87b073348e606935ba900ab4b05bb9e8f4d83fd0
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-03-25 21:01:10 +1100

    feat: connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('DB Connected');
+}).catch((err) => {
+    console.log('DB Connection Error: ', err);
+});
 
 const app = express();
 
