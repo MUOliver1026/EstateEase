c3c24ce909c15eab81711239d153926b99037285
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-06-13 00:25:23 +1000

    complete create listing page ui

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
-        <StoreProvider>
-          <Header />
-          <main>{children}</main>
-        </StoreProvider>
-      </body>
-    </html>
+      <html lang="en" className="dark">
+        <body className={inter.className}>
+          <StoreProvider>
+            <Header />
+            <main>{children}</main>
+          </StoreProvider>
+        </body>
+      </html>
   );
 }
--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -11,7 +11,7 @@ import { updateUserStart, updateUserSuccess, updateUserFailure,
   deleteUserStart, deleteUserSuccess, deleteUserFailure,
   signOutUserStart, signOutUserSuccess, signOutUserFailure } from '@/lib/user/userSlice';
 import { useDispatch } from 'react-redux';
-import router from 'next/router';
+import Link from 'next/link';
 
 const Profile = () => {
   const router = useRouter();
@@ -203,6 +203,9 @@ const Profile = () => {
           >
             {loading ? 'Loading...' : 'Update'}
           </button>
+          <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-75' href={"/create-listing"}>
+            Create Listing
+          </Link>
         </form>
         <div className='flex justify-between mt-5'>
           <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
