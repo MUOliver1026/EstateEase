1a9e242bb7c83de567db8947903ab9832bca8f14
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-06-12 23:19:08 +1000

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -43,6 +44,7 @@ app.listen(8000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 // middleware to handle errors
 
